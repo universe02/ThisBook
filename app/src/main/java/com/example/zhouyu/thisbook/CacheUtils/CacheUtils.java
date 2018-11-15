0c6ddc1a92547b52757d9bedf0366129cda72807
universe02 <yuz6688@gmail.com>
2018-11-14 23:02:02 -0500

    R solved

--- app/src/main/java/com/example/zhouyu/thisbook/CacheUtils/CacheUtils.java
+++ app/src/main/java/com/example/zhouyu/thisbook/CacheUtils/CacheUtils.java
@@ -6,7 +6,7 @@ import android.content.SharedPreferences;
 import com.example.zhouyu.thisbook.SplashActivity;
 
 public class CacheUtils {
-    public static boolean getBoolean(Context context, String Key) {
+    public static boolean getBoolean(Context context, String key) {
         SharedPreferences sp = context.getSharedPreferences("test", Context.MODE_PRIVATE);
         return sp.getBoolean(key, false);
     }
--- app/src/main/java/com/example/zhouyu/thisbook/R.java
+++
@@ -1,4 +0,0 @@
-package com.example.zhouyu.thisbook;
-
-public class R {
-}
